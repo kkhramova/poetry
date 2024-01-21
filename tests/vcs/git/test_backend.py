9546506adad8dea7be6ad57edd738fbd95a26131
Kateryna Khramova <khramova.kv@gmail.com>
2024-01-21 14:11:31 -0500

    Test annotated_tag function

--- tests/vcs/git/test_backend.py
+++ tests/vcs/git/test_backend.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from poetry.vcs.git.backend import is_revision_sha
 
+from poetry.vcs.git.backend import is_revision_sha, annotated_tag, GitRefSpec
 
 VALID_SHA = "c5c7624ef64f34d9f50c3b7e8118f7f652fddbbd"
 
@@ -24,3 +24,14 @@ def test_invalid_revision_sha_min_len() -> None:
 def test_invalid_revision_sha_max_len() -> None:
     result = is_revision_sha(VALID_SHA + "42")
     assert result is False
+
+
+def test_annotated_tag_with_string_input() -> None:
+    result = annotated_tag("c5c7624ef64f3")
+    assert result == b"c5c7624ef64f3^{}"
+
+
+def test_annotated_tag_with_bytes_input() -> None:
+    result = annotated_tag(b"c5c7624ef64f3")
+    assert result == b"c5c7624ef64f3^{}"
+
